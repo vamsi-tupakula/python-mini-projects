3e82711ea5e273a35ba18bc7f787888363a6123c
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-07-23 18:14:16 +0530

    hangman done

--- hangman.py
+++ hangman.py
@@ -1,14 +1,43 @@
+from tokenize import String
 import requests
 import json
 import random
+import string
 
 def get_random_word():
+    # fetching api
     response_API = requests.get('https://www.randomlists.com/data/words.json')
     data = response_API.text
+
+    # coverting it into json
     json_data = json.loads(data)
     words = json_data["data"]
 
-    return random.choice(words)
+    return random.choice(words).upper()
 
-random_word = get_random_word()
-print(random_word)
+def hangman():
+    word = get_random_word()
+    word_letters = set(word)
+    alphabets = set(string.ascii_uppercase)
+    user_letters = set() # letters entered by user
+
+    while(len(word_letters) > 0):
+        print('You have used : ', ' '.join(user_letters))
+
+        word_list = [letter if letter in user_letters else '-' for letter in word]
+        print('current word : ', " ".join(word_list))
+
+        user_letter = input('Guess an alphabet : ').upper()
+        if user_letter in alphabets - user_letters:
+            user_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+        elif user_letter in user_letters:
+            print('you have already used it..')
+        else:
+            print("You entered a invalid char...")
+
+    print('final word : ', " ".join(word))    
+
+
+hangman()
